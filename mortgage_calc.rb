7091a352c9758a7a9ad326b08fb9f96a8352d649
Andrew Harris <blakebharris@gmail.com>
2016-07-12 19:57:52 -0500

    finished mortgage calc

--- mortgage_calc.rb
+++ mortgage_calc.rb
@@ -1,34 +1,78 @@
-#get loan amount, APR, loan duration in years
-#calculate monthly int rate, and loan duration in months
+# get loan amount, APR, loan duration in years
+# calculate monthly int rate, and loan duration in months
+
+# Take rate from yearly to monthly
 def monthly_interest_rate(apr)
-  apr = apr / 12
-  return apr.to_f
+  apr = (apr / 100.to_f) / 12
+  apr.to_f
 end
 
+def prompt(str)
+  puts "=>#{str}"
+end
+
+# takes the years and makes them into monthly periods
 def years_to_month(years)
   periods = years * 12.0
-  return periods.to_f
-end
-fixed_payment = 0
-def calculate_loan_amount(rate, n_periods, total_loan_amount)
-  fixed_payment = (rate/ (1-((1+rate) ** (n_periods * -1)))) * total_loan_amount
-  return fixed_payment.to_f
+  periods.to_f
 end
 
-puts "Welcome to loan calculator..."
-puts "Can you provide: Loan Amount?"
-loan_amount = gets.chomp.to_f#check to see if valid int
+# calculate fixed loan payment
+def calculate_loan(rate, n_periods, total_loan_amount)
+  fixed_payment = (rate /
+                  (1 - ((1 + rate)**(n_periods * - 1)))) * total_loan_amount
 
+  fixed_payment.to_f
+end
 
-puts "Can you provide: APR in whole percent 5 = 5%"
-annual_percentage_rate = gets.chomp.to_f
-monthly_rate = monthly_interest_rate(annual_percentage_rate)
+loop do
+  prompt('Welcome to loan calculator...')
+  prompt('Can you provide: Loan Amount?')
 
+  # check for valid input
+  loan_amount = ''
+  loop do
+    loan_amount = gets.chomp
+    if loan_amount.empty? || loan_amount.to_f < 0
+      prompt('You must enter a valid, positive, number')
+    else
+      break
+    end
+  end
 
-puts "Can you provide: Loan Duration in years"
-loan_duration = gets.chomp.to_i
-months = years_to_month(loan_duration)
+  prompt('Can you provide: APR in whole percent 5 = 5%')
+  # check for valid input
+  annual_rate = ''
+  loop do
+    annual_rate = gets.chomp
+    if annual_rate.empty? || annual_rate.to_f < 0
+      prompt('You must enter a valid, positive, number')
+    else
+      break
+    end
+  end
 
-fixed_loan = calculate_loan_amount(monthly_rate, months, loan_amount)
+  monthly_rate = monthly_interest_rate(annual_rate.to_f)
 
-p fixed_loan
+  prompt('Can you provide: Loan Duration in years')
+
+  loan_duration = ''
+  loop do
+    loan_duration = gets.chomp
+    if loan_duration.empty? || loan_duration.to_i < 0
+      prompt('You must enter a valid, positive, number')
+    else
+      break
+    end
+  end
+
+  months = years_to_month(loan_duration.to_f)
+
+  fixed_loan = calculate_loan(monthly_rate.to_f, months.to_f, loan_amount.to_f)
+  prompt("PMT: #{fixed_loan.round(2)}
+  YEARS: #{loan_duration}
+  RATE: #{annual_rate}%")
+  prompt('Would you like to experiment some more? y/n')
+  end_res = gets.chomp.downcase
+  break unless end_res.downcase.start_with?('y')
+end
